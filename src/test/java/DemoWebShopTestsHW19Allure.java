de42ce7b14a73d052806caa1b72c9b44edb70476
cbet <npocbet@gmail.com>
2022-01-14 23:34:50 +0300

    hw19 in done, Added DemoWebShopTestsHW19Allure.java - its the same with DemoWebShopTestsHW15Owner.java but Allure added and some custom templates.

--- src/test/java/DemoWebShopTestsHW19Allure.java
+++ src/test/java/DemoWebShopTestsHW19Allure.java
@@ -1,6 +1,8 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import config.APIConfig;
+import filters.CustomLogFilter;
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.Test;
@@ -19,57 +21,12 @@ import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
-public class DemoWebShopTestsHW15 {
+public class DemoWebShopTestsHW19Allure {
+
+    public String cookie;
 
     @Test
-    void addToWishlistTestLocal() {
-        APIConfig apiConfig = ConfigFactory.create(APIConfig.class, System.getProperties());
-
-        RestAssured.baseURI = apiConfig.getBaseUrl(); //"http://demowebshop.tricentis.com";
-        Configuration.baseUrl = apiConfig.getBaseUrl(); //"http://demowebshop.tricentis.com";
-
-        String cookie = given()
-                .contentType("application/x-www-form-urlencoded")
-                .formParam("Email", apiConfig.getLogin()) // "npocbet@gmail.com"
-                .formParam("Password", apiConfig.getPassword()) // "qwerpoiu"
-                .when()
-                .post("/login")
-                .then()
-                .extract().cookie("NOPCOMMERCE.AUTH");
-
-        given()
-                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                .cookie("NOPCOMMERCE.AUTH", cookie)
-                .body("giftcard_1.RecipientName=Some" +
-                        "&giftcard_1.RecipientEmail=man%40man.man" +
-                        "&giftcard_1.SenderName=Svjato+Krravts" +
-                        "&giftcard_1.SenderEmail=npocbet%40gmail.com" +
-                        "&giftcard_1.Message=&" +
-                        "addtocart_1.EnteredQuantity=1")
-                .when()
-                .post("http://demowebshop.tricentis.com/addproducttocart/details/1/2")
-                .then()
-                .statusCode(200)
-                .body("success", is(true));
-
-        step("Open wishlist", () -> {
-            open("http://demowebshop.tricentis.com/");
-            getWebDriver().manage().addCookie(new Cookie("NOPCOMMERCE.AUTH", cookie));
-            open("http://demowebshop.tricentis.com/wishlist");
-        });
-
-        step("check there is and item in the wishlist", () -> {
-            $(".page-body").shouldHave(Condition.text("$5 Virtual Gift Card"));
-        });
-    }
-
-    @Test
-    void addToWishlistTestRemote() throws IOException {
-
-        String loginAndPassword = "login=npocbet@gmail.com\npassword=qwerpoiu";
-        Path properties = Paths.get("/tmp/auth.properties");
-
-        Files.write(properties, loginAndPassword.getBytes(StandardCharsets.UTF_8));
+    void addToWishlistTestLocalWithListener() {
 
         APIConfig apiConfig = ConfigFactory.create(APIConfig.class, System.getProperties());
 
@@ -86,6 +43,7 @@ public class DemoWebShopTestsHW15 {
                 .extract().cookie("NOPCOMMERCE.AUTH");
 
         given()
+                .filter(new AllureRestAssured())
                 .contentType("application/x-www-form-urlencoded; charset=UTF-8")
                 .cookie("NOPCOMMERCE.AUTH", cookie)
                 .body("giftcard_1.RecipientName=Some" +
@@ -95,8 +53,11 @@ public class DemoWebShopTestsHW15 {
                         "&giftcard_1.Message=&" +
                         "addtocart_1.EnteredQuantity=1")
                 .when()
+                .log().uri()
+                .log().headers()
                 .post("http://demowebshop.tricentis.com/addproducttocart/details/1/2")
                 .then()
+                .log().body()
                 .statusCode(200)
                 .body("success", is(true));
 
@@ -109,7 +70,57 @@ public class DemoWebShopTestsHW15 {
         step("check there is and item in the wishlist", () -> {
             $(".page-body").shouldHave(Condition.text("$5 Virtual Gift Card"));
         });
-
-        Files.delete(properties);
     }
+    @Test
+    void addToWishlistTestLocalWithTemplates() {
+
+
+        APIConfig apiConfig = ConfigFactory.create(APIConfig.class, System.getProperties());
+
+        RestAssured.baseURI = apiConfig.getBaseUrl(); //"http://demowebshop.tricentis.com";
+        Configuration.baseUrl = apiConfig.getBaseUrl(); //"http://demowebshop.tricentis.com";
+
+        step("Get cookie", () -> {
+            cookie = given()
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("Email", apiConfig.getLogin()) // "npocbet@gmail.com"
+                .formParam("Password", apiConfig.getPassword()) // "qwerpoiu"
+                .when()
+                .post("/login")
+                .then()
+                .extract().cookie("NOPCOMMERCE.AUTH");
+        });
+
+        step("Send API request", () -> {
+            given()
+                .filter(new CustomLogFilter().withCustomTemplates())
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .cookie("NOPCOMMERCE.AUTH", cookie)
+                .body("giftcard_1.RecipientName=Some" +
+                        "&giftcard_1.RecipientEmail=man%40man.man" +
+                        "&giftcard_1.SenderName=Svjato+Krravts" +
+                        "&giftcard_1.SenderEmail=npocbet%40gmail.com" +
+                        "&giftcard_1.Message=&" +
+                        "addtocart_1.EnteredQuantity=1")
+                .when()
+                .log().uri()
+                .log().headers()
+                .post("http://demowebshop.tricentis.com/addproducttocart/details/1/2")
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("success", is(true));
+        });
+
+        step("Open wishlist", () -> {
+            open("http://demowebshop.tricentis.com/");
+            getWebDriver().manage().addCookie(new Cookie("NOPCOMMERCE.AUTH", cookie));
+            open("http://demowebshop.tricentis.com/wishlist");
+        });
+
+        step("check there is and item in the wishlist", () -> {
+            $(".page-body").shouldHave(Condition.text("$5 Virtual Gift Card"));
+        });
+    }
+
 }
