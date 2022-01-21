a38fa6dca7ac86a70422f8d61834121ecd3630ab
npocbet <npocbet@gmail.com>
2022-01-21 07:27:26 +0300

    fixed bug - dependency "org.aspectj:aspectjweaver:1.9.6" was needed.

--- src/test/java/config/APIConfig.java
+++ src/test/java/config/APIConfig.java
@@ -19,7 +19,7 @@ public interface APIConfig extends Config {
     String getPassword();
 
     @Key("browser")
-    @DefaultValue("firefox")
+    @DefaultValue("chrome")
     String getBrowser();
 
     @Key("browserVersion")
